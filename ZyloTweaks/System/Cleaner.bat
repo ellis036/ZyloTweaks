7b2c691658b3a79c25bf793946014d91a9086487
ellis036 <daredignify@gmail.com>
2024-10-24 19:53:51 +0100

    Update Cleaner.bat

--- ZyloTweaks/System/Cleaner.bat
+++ ZyloTweaks/System/Cleaner.bat
@@ -1,4 +1,5 @@
 @echo off
+
 echo Cleaning NVIDIA GLCache folder...
 set "nvidiaGLCache=%LOCALAPPDATA%\NVIDIA\GLCache"
 if exist "%nvidiaGLCache%" (
@@ -7,6 +8,7 @@ if exist "%nvidiaGLCache%" (
 ) else (
     echo GLCache folder not found.
 )
+
 echo Cleaning Temp folder...
 set "tempFolder=%LOCALAPPDATA%\Temp"
 if exist "%tempFolder%" (
@@ -15,15 +17,19 @@ if exist "%tempFolder%" (
 ) else (
     echo Temp folder not found.
 )
+
 echo Cleaning C:\Windows\Temp folder...
 del /q /s "C:\Windows\Temp\*.*"
 echo Contents of C:\Windows\Temp folder have been deleted.
+
 echo Cleaning C:\Windows\Prefetch folder...
 del /q /s "C:\Windows\Prefetch\*.*"
 echo Contents of C:\Windows\Prefetch folder have been deleted.
+
 echo Cleaning system log files...
 for /F "tokens=*" %%G in ('wevtutil el') DO wevtutil cl "%%G"
 echo System log files have been cleared.
+
 echo Cleaning Internet Explorer cache...
 RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
 echo Internet Explorer cache has been cleared.
@@ -33,18 +39,25 @@ net stop wuauserv
 rd /s /q "%windir%\SoftwareDistribution\DataStore"
 net start wuauserv
 echo Windows Update cache has been cleared.
+
 echo Cleaning Windows Error Reporting files...
 del /q /s "%LOCALAPPDATA%\Microsoft\Windows\WER\ReportQueue\*"
 echo Windows Error Reporting files have been deleted.
+
 echo Cleaning Windows temporary files...
 del /q /s "%windir%\Temp\*"
 echo Windows temporary files have been deleted.
+
 echo Cleaning Windows memory dump files...
 del /q /s "%windir%\Minidump\*"
 echo Windows memory dump files have been deleted.
+
 echo Cleaning Windows logs...
 for /D %%i in ("%windir%\Logs\*") do (
-    rmdir /s /q "%%i")
+    rmdir /s /q "%%i"
+)
 echo Windows logs have been cleared.
+
 echo Cleanup complete.
+
 pause
