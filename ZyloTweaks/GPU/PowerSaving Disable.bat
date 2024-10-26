5a66549974e43adc8cf7f67c25a5ef8ff9368484
ellis036 <daredignify@gmail.com>
2024-10-26 15:16:32 +0100

    Update PowerSaving Disable.bat

--- ZyloTweaks/GPU/PowerSaving Disable.bat
+++ ZyloTweaks/GPU/PowerSaving Disable.bat
@@ -2,4 +2,4 @@
 echo Applying 'Lowest Input Latency Possible'...
     reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v PStateTime /t REG_DWORD /d 0x01000001 /f
     echo Value applied: 01000001
-    pause
+    exit
