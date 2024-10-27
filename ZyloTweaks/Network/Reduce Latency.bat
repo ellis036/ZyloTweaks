ea474dbe9b11403a492db0f32aefab88d867e401
ellis036 <daredignify@gmail.com>
2024-10-27 19:08:10 +0000

    Update Reduce Latency.bat

--- ZyloTweaks/Network/Reduce Latency.bat
+++ ZyloTweaks/Network/Reduce Latency.bat
@@ -39,4 +39,5 @@ for /f %%i in ('wmic path win32_networkadapter get GUID ^| findstr "{"') do reg 
 for /f %%i in ('wmic path win32_networkadapter get GUID ^| findstr "{"') do reg add "HKLM\System\CurrentControlSet\services\Tcpip\Parameters\Interfaces\%%i" /v "TcpDelAckTicks" /t REG_DWORD /d "0" /f >nul 2>&1
 for /f %%i in ('wmic path win32_networkadapter get GUID ^| findstr "{"') do reg add "HKLM\System\CurrentControlSet\services\Tcpip\Parameters\Interfaces\%%i" /v "TCPNoDelay" /t REG_DWORD /d "1" /f >nul 2>&1
 echo.
+POWERSHELL.EXE -Command "Add-Type -AssemblyName System.Windows.Forms; [void] [System.Windows.Forms.MessageBox]::Show( 'Tweaks Applied Successfully', 'Success', 'OK', 'Information' )"
 exit
