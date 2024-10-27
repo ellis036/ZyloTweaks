d7f8fb79b9efe97b60de058f1c055e9d76786908
ellis036 <daredignify@gmail.com>
2024-10-27 19:06:04 +0000

    Update Enable High-Definition Audio.bat

--- ZyloTweaks/Devices/Enable High-Definition Audio.bat
+++ ZyloTweaks/Devices/Enable High-Definition Audio.bat
@@ -4,4 +4,5 @@ for /r %SystemRoot% %%a in (*.exe) do (
     fsutil behavior set PrefetchParameters 0
 )
 echo Done applying /PREFETCH:0 to all programs.
+POWERSHELL.EXE -Command "Add-Type -AssemblyName System.Windows.Forms; [void] [System.Windows.Forms.MessageBox]::Show( 'Tweaks Applied Successfully', 'Success', 'OK', 'Information' )"
 exit
