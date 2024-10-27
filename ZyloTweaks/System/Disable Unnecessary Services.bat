9955d57c93767d18af9f98be0e12ec27acad5f9b
ellis036 <daredignify@gmail.com>
2024-10-27 19:05:15 +0000

    Update Disable Unnecessary Services.bat

--- ZyloTweaks/System/Disable Unnecessary Services.bat
+++ ZyloTweaks/System/Disable Unnecessary Services.bat
@@ -152,4 +152,5 @@ taskkill /f /t /IM ScreenClippingHost.exe
 rmdir /S /Q "%windir%\Windows\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe"
 taskkill /f /t /IM TextInputHost.exe
 rmdir /S /Q "%windir%\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\TextInputHost.exe"
+POWERSHELL.EXE -Command "Add-Type -AssemblyName System.Windows.Forms; [void] [System.Windows.Forms.MessageBox]::Show( 'Tweaks Applied Successfully', 'Success', 'OK', 'Information' )"
 exit
