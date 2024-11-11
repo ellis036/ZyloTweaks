67197d26eca9272aafd9a0cd185addbbb5c27fd7
ellis036 <daredignify@gmail.com>
2024-11-11 15:38:43 -0800

    Update Tweak Network.bat

--- ZyloTweaks/Network/Tweak Network.bat
+++ ZyloTweaks/Network/Tweak Network.bat
@@ -9,14 +9,6 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v UseVSync /t REG_DWOR
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v BufferCount /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v PresentationInterval /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v FrameLatency /t REG_DWORD /d 1 /f
-
-echo "Low Latency settings applied successfully."
-pause
-goto Menu
-
-:: Function to apply Smooth settings
-:Smooth
-echo Applying Smooth settings...
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v MaxBufferCount /t REG_DWORD /d 2 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v MaxFrameLatency /t REG_DWORD /d 2 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v FPUPreserve /t REG_DWORD /d 0 /f
@@ -26,6 +18,5 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v BufferCount /t REG_D
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v PresentationInterval /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D" /v FrameLatency /t REG_DWORD /d 2 /f
 
-echo "Smooth settings applied successfully."
 POWERSHELL.EXE -Command "Add-Type -AssemblyName System.Windows.Forms; [void] [System.Windows.Forms.MessageBox]::Show( 'Tweaks Applied Successfully', 'Success', 'OK', 'Information' )"
 exit
