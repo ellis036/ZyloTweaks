9b1de5f255d65a7806045dec2898bb02a4e2648d
ellis036 <daredignify@gmail.com>
2024-11-11 15:37:17 -0800

    Update Reduce Latency.bat

--- ZyloTweaks/Network/Reduce Latency.bat
+++ ZyloTweaks/Network/Reduce Latency.bat
@@ -20,11 +20,6 @@ netsh int ip set global neighborcachelimit=4096
 netsh int ip set global routecachelimit=4096
 netsh int ip set global sourceroutingbehavior=drop
 
-PowerShell Disable-NetAdapterLso -Name "*"
-powershell "ForEach($adapter In Get-NetAdapter){Disable-NetAdapterPowerManagement -Name $adapter.Name -ErrorAction SilentlyContinue}"
-powershell "ForEach($adapter In Get-NetAdapter){Disable-NetAdapterLso -Name $adapter.Name -ErrorAction SilentlyContinue}"
-
-
 Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "Tcp1323Opts" /t REG_DWORD /d "0" /f
 Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "TcpMaxDupAcks" /t REG_DWORD /d "2" /f
 Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "TcpTimedWaitDelay" /t REG_DWORD /d "32" /f
