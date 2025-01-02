2df3f962d4b34df61d8427d015aba2df5c693486
ellis036 <daredignify@gmail.com>
2025-01-02 16:41:24 +0000

    Delete ZyloTweaks directory

--- ZyloTweaks/Devices/Enable High-Definition Audio.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-for /r %SystemRoot% %%a in (*.exe) do (
-    echo Applying /PREFETCH:0 to "%%a"
-    fsutil behavior set PrefetchParameters 0
-)
-echo Done applying /PREFETCH:0 to all programs.
-POWERSHELL.EXE -Command "Add-Type -AssemblyName System.Windows.Forms; [void] [System.Windows.Forms.MessageBox]::Show( 'Tweaks Applied Successfully', 'Success', 'OK', 'Information' )"
-exit
--- ZyloTweaks/GPU/PowerSaving Disable.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Applying 'Lowest Input Latency Possible'...
-    reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v PStateTime /t REG_DWORD /d 0x01000001 /f
-    echo Value applied: 01000001
-POWERSHELL.EXE -Command "Add-Type -AssemblyName System.Windows.Forms; [void] [System.Windows.Forms.MessageBox]::Show( 'Tweaks Applied Successfully', 'Success', 'OK', 'Information' )"
-exit
